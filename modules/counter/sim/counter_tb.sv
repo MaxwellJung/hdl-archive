6465d891bfeb31d028d5d7561f3f8a39fccd4a7c
Maxwell Jung <jungmaxwell@gmail.com>
2026-06-13 21:15:04 -0700

    test: better counter tb

--- modules/counter/sim/counter_tb.sv
+++ modules/counter/sim/counter_tb.sv
@@ -39,28 +39,34 @@ module counter_tb;
     repeat (10) @(posedge clk);
     rst = 0;
 
+    // Wait for 10 clock cycles.
+    repeat (10) @(posedge clk);
+
+    assert (count == 0)
+      else $error("Counter should be zero after reset");
 
     // start counter for 50 ns
-    repeat (10) @(posedge clk);
+    @(posedge clk);
     en = 1;
     repeat (5) @(posedge clk);
     en = 0;
 
-    assert (count != 0)
-      else $error("Counter should not be zero after rst is deasserted");
+    assert (count == 5)
+      else $error("Counter should be 5 after counting for 5 clock cycles");
 
-    // resume counter for 50 ns
-    repeat (10) @(posedge clk);
+    // resume counter for 120 ns
+    @(posedge clk);
     en = 1;
-    repeat (5) @(posedge clk);
+    repeat (12) @(posedge clk);
     en = 0;
 
-    // hold rst for 50 ns
-    rst = 1;
-    repeat (5) @(posedge clk);
-    rst = 0;
+    assert (count == 17)
+      else $error("Counter should be 17 after counting for 12 more clock cycles");
 
     repeat (20) @(posedge clk);
+
+    assert (count == 17)
+      else $error("Counter should remain at 17 when disabled");
     $finish;
   end
 
